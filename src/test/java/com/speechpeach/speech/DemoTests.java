0243ca1c8f555dfd1ba8f173f1dca1bf282f4937
Kim-AYoung <onionlily123@naver.com>
2024-04-09 16:37:10 +0900

    test: CI 테스트를 위한 데모 테스트 추가

---
+++ src/test/java/com/speechpeach/speech/DemoTests.java
@@ -0,0 +1,21 @@
+package com.speechpeach.speech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@SpringBootTest
+class DemoTests {
+
+    @Test
+    void success() {
+        assertTrue(true);
+    }
+
+    @Test
+    void failed() {
+        assertTrue(false);
+    }
+}
+
