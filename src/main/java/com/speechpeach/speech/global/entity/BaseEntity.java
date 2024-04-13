3f1cae186ca84153efd0d00837acbf28437ef669
방소영 <bjo8244@naver.com>
2024-04-13 15:53:54 +0900

    setting: BaseEntity 생성
    1. 모든 클래스에 공통 속성인 생성일시, 수정일시, 데이터삭제여부 관리하는 BaseEntity 생성

--- src/main/java/com/speechpeach/speech/global/entity/BaseEntity.java
+++ src/main/java/com/speechpeach/speech/global/entity/BaseEntity.java
@@ -5,14 +5,24 @@ import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
 import lombok.Getter;
 import org.hibernate.annotations.ColumnDefault;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
-import java.util.UUID;
+import java.time.LocalDateTime;
 
 @Getter
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener.class)
-public class BaseEntity extends BaseTimeEntity {
+public abstract class BaseEntity {
+
+    @CreatedDate
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime createdDate;
+
+    @LastModifiedDate
+    @Column(nullable = false)
+    private LocalDateTime modifiedDate;
 
     @ColumnDefault("false")
     @Column(columnDefinition = "TINYINT(1)")
