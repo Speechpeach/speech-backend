36308bab9fc804920d8dca46678c8a4e406a120e
방소영 <bjo8244@naver.com>
2024-04-16 09:13:04 +0900

    setting: BaseEntity 생성
    1. 모든 클래스에 공통 속성인 생성일시, 수정일시, 데이터삭제여부 관리하는 BaseEntity 생성
    2. 각 컬럼에 값 제대로 들어가는지 test 성공
    3. test db를 인메모리 h2 db 연결로 수정

--- src/test/java/com/speechpeach/speech/global/entity/BaseEntityTest.java
+++ src/test/java/com/speechpeach/speech/global/entity/BaseEntityTest.java
@@ -3,7 +3,6 @@ package com.speechpeach.speech.global.entity;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
@@ -14,13 +13,12 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 @ExtendWith(SpringExtension.class)
 @DataJpaTest
-@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 @DisplayName("BaseEntity 테스트")
 class BaseEntityTest {
 
     @Test
     @DisplayName("생성일시, 수정일시, 데이터삭제여부 잘 들어가는지 확인")
-    void BaseEntitySetTest() throws NoSuchFieldException, IllegalAccessException{
+    void baseEntitySetTest() throws NoSuchFieldException, IllegalAccessException{
         // given
         LocalDateTime testTime = LocalDateTime.now();
         BaseEntity baseEntity = new BaseEntity() {};
