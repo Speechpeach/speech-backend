8c8658abb1e0fafbf1f028400942809a641664a1
beautiflow <1114ny@naver.com>
2024-04-25 17:26:34 +0900

    feat: create MentoringPossibleDate Entity

---
+++ src/main/java/com/speechpeach/speech/mento/entity/MentoringPossibleDate.java
@@ -0,0 +1,41 @@
+package com.speechpeach.speech.mento.entity;
+
+import com.speechpeach.speech.global.entity.BaseEntity;
+import com.speechpeach.speech.user.entity.Member;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Entity
+@Table(name = "mentoring_possible_dates")
+public class MentoringPossibleDate extends BaseEntity {
+
+    @Id
+    @Column(name = "mentoring_possible_date_id", nullable = false)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long mentoringPossibleDateId;
+
+    @Column(name = "mentoring_possible_date_day")
+    private String mentoringPossibleDateDay;
+
+    @Column(name = "mentoring_possible_date_time_id")
+    private String mentoringPossibleDateTimeId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "mento_id", nullable = false)
+    private Mento mentoId;
+}
