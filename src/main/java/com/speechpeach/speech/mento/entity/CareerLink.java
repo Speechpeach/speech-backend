f96e7e2d1f54dea611c36ac77adef092d7578cf4
beautiflow <1114ny@naver.com>
2024-04-25 17:25:28 +0900

    feat: create CareerLink Entity

---
+++ src/main/java/com/speechpeach/speech/mento/entity/CareerLink.java
@@ -0,0 +1,39 @@
+package com.speechpeach.speech.mento.entity;
+
+import com.speechpeach.speech.global.entity.BaseEntity;
+import com.speechpeach.speech.user.entity.Member;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Entity
+@Table(name = "career_links")
+public class CareerLink extends BaseEntity {
+
+    @Id
+    @Column(name = "career_link_id", nullable = false)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long careerLinkId;
+
+    @Column(name ="career_link_content", nullable = false)
+    private String careerLinkContent;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "mento_id", nullable = false)
+    private Mento mentoId;
+}
