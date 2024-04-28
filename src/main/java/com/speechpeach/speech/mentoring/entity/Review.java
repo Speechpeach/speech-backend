a614aea2025a625437f6ac35b995b4bb040771f7
beautiflow <1114ny@naver.com>
2024-04-29 00:58:43 +0900

    feat: create Review Entity

---
+++ src/main/java/com/speechpeach/speech/mentoring/entity/Review.java
@@ -0,0 +1,43 @@
+package com.speechpeach.speech.mentoring.entity;
+
+import com.speechpeach.speech.global.entity.BaseEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+@Entity
+@Table(name = "reviews")
+public class Review extends BaseEntity {
+
+    @Id
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "mentoring_apply_id", nullable = false)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private MentoringApplication mentoringApplication;
+
+    @Column(name = "review_star",columnDefinition = "TINYINT(3)", nullable = false)
+    private int reviewStar;
+
+    @Column(name = "review_content", columnDefinition = "TEXT", length = 500, nullable = false)
+    private String reviewContent;
+
+    private Review(int reviewStar, String reviewContent){
+        this.reviewStar = reviewStar;
+        this.reviewContent = reviewContent;
+    }
+
+    public static Review createReview(int reviewStar, String reviewContent){
+        return new Review(reviewStar, reviewContent);
+    }
+}
