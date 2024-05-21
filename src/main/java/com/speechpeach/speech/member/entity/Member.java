ea83e599c13e9e35732e5c9dc97c2a907397ff40
Kim-AYoung <onionlily123@naver.com>
2024-05-21 11:28:41 +0900

    refactor: User 엔티티 제거 후, Member 엔티티 생성

---
+++ src/main/java/com/speechpeach/speech/member/entity/Member.java
@@ -0,0 +1,45 @@
+package com.speechpeach.speech.member.entity;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+@Entity
+@Table(name = "members")
+public class Member {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID memberId;
+
+    @Column(nullable = false)
+    private String socialLoginProvider;
+
+    @Column(nullable = false)
+    private String socialLoginId;
+
+    @Column(name = "member_nickname", nullable = false)
+    private String nickname;
+
+    @Column(name = "member_profile_image_url", nullable = false)
+    private String profileImageUrl;
+
+    @Column(name = "member_email", nullable = false)
+    private String email;
+
+    @Builder
+    public Member(String socialLoginProvider, String socialLoginId, String nickname, String profileImageUrl, String email) {
+        this.socialLoginProvider = socialLoginProvider;
+        this.socialLoginId = socialLoginId;
+        this.nickname = nickname;
+        this.profileImageUrl = profileImageUrl;
+        this.email = email;
+    }
+}
+
